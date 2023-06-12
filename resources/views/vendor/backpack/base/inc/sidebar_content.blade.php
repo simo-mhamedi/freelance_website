e50b34971400ae139c8f0b8ed1c3513ec99f7952
Mohamed Mhamdi <Mohamed.Mhamdi@fan4all.com>
2023-06-12 17:02:44 +0100

    update

---
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,9 @@
+{{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i> Users</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('request') }}"><i class="nav-icon la la-file-image-o"></i> Requests</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('estimate') }}"><i class="nav-icon la la-file-text-o"></i> Estimates</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user-categorie') }}"><i class="nav-icon la la-group"></i> User categories</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('categorie') }}"><i class="nav-icon la la la-th-large"></i> Categories</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('sub-categorie') }}"><i class="nav-icon la la la-th-list"></i> Sub categories</a></li>
---
+++ routes/backpack/custom.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// --------------------------
+// Custom Backpack Routes
+// --------------------------
+// This route file is loaded automatically by Backpack\Base.
+// Routes you generate using Backpack\Generators will be placed here.
+
+Route::group([
+    'prefix'     => config('backpack.base.route_prefix', 'admin'),
+    'middleware' => array_merge(
+        (array) config('backpack.base.web_middleware', 'web'),
+        (array) config('backpack.base.middleware_key', 'admin')
+    ),
+    'namespace'  => 'App\Http\Controllers\Admin',
+], function () { // custom admin routes
+    Route::crud('user', 'UserCrudController');
+    Route::crud('estimate', 'EstimateCrudController');
+    Route::crud('user-categorie', 'UserCategorieCrudController');
+    Route::crud('categorie', 'CategorieCrudController');
+    Route::crud('sub-categorie', 'SubCategorieCrudController');
+    Route::crud('request', 'RequestCrudController');
+    Route::get('charts/weekly-users', 'Charts\WeeklyUsersChartController@response')->name('charts.weekly-users.index');
+}); // this should be the absolute last line of this file
